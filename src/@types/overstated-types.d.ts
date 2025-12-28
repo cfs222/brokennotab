7e983b9ca0387bda7b9fc669655136e9f4c7a853
Omar <omar@example.com>
2025-12-28 14:09:00 +0200

    Replace backup-before-changes with current local project

---
+++ src/@types/overstated-types.d.ts
@@ -0,0 +1,6 @@
+// src/types/overstated-types.d.ts
+declare module "overstated" {
+  export const Container: any;
+  export const autosuspend: any;
+  // All other exports will be automatically inferred
+}
--- src/renderer/containers/main/tutorial.ts
+++ src/renderer/containers/main/tutorial.ts
@@ -1,74 +1,62 @@
-
 /* IMPORT */
 
-import {shell} from 'electron';
-import Dialog from 'electron-dialog';
-import * as unzip from 'extract-zip';
-import {Container, autosuspend} from 'overstated';
-import * as path from 'path';
-import * as pify from 'pify';
-import pkg from '@root/package.json';
+import { shell } from "electron";
+import Dialog from "electron-dialog";
+import * as unzip from "extract-zip";
+import { Container, autosuspend } from "overstated";
+import * as path from "path";
+import * as pify from "pify";
+import pkg from "@root/package.json";
 
 /* TUTORIAL */
 
 class Tutorial extends Container<TutorialState, MainCTX> {
-
   /* CONSTRUCTOR */
 
-  constructor () {
+  constructor() {
+    super();
 
-    super ();
-
-    autosuspend ( this );
-
+    autosuspend(this);
   }
 
   /* API */
 
   import = async () => {
+    const cwd = this.ctx.cwd.get();
 
-    const cwd = this.ctx.cwd.get ();
+    if (!cwd) return;
 
-    if ( !cwd ) return;
-
-    const tutorialPath = path.join ( __static, 'tutorial.zip' );
+    const tutorialPath = path.join(__static, "tutorial.zip");
 
     try {
-
-      await pify ( unzip )( tutorialPath, { dir: cwd } );
-
-    } catch ( e ) {
-
-      Dialog.alert ( 'Failed to import the tutorial notes, please report the issue' );
-
+      await pify(unzip)(tutorialPath, { dir: cwd });
+    } catch (e) {
+      Dialog.alert(
+        "Failed to import the tutorial notes, please report the issue"
+      );
     }
-
-  }
+  };
 
   open = () => {
-
-    shell.openExternal ( pkg['tutorial'].url );
-
-  }
+    shell.openExternal(pkg["tutorial"].url);
+  };
 
   dialog = () => {
+    const choice = Dialog.choice(
+      "Do you want to browse the tutorial online or do you want to import all the notes?",
+      ["Cancel", "Import Notes", "Online Tutorial"]
+    );
 
-    const choice = Dialog.choice ( 'Do you want to browse the tutorial online or do you want to import all the notes?', ['Cancel', 'Import Notes', 'Online Tutorial'] );
-
-    switch ( choice ) {
-
+    switch (choice) {
       case 1:
-        this.import ();
+        this.import();
         break;
 
       case 2:
-        this.open ();
+        this.open();
         break;
-
     }
-
-  }
-
+  };
 }
 
 /* EXPORT */
